62836a1ce13560f12a505098cb4be9b5bc1c58d0
alexvolchkov <volkaa@mail.ru>
2022-06-23 10:03:22 +0300

    4. Категории в TODO List [#331991 #315483]

--- src/main/java/ru/job4j/todo/model/Item.java
+++ src/main/java/ru/job4j/todo/model/Item.java
@@ -2,9 +2,7 @@ package ru.job4j.todo.model;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "items")
@@ -22,7 +20,7 @@ public class Item {
     @JoinColumn(name = "user_id")
     private User user;
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    private List<Category> categories = new ArrayList<>();
+    private Set<Category> categories = new HashSet<>();
 
     public Item() {
     }
@@ -88,11 +86,11 @@ public class Item {
         this.user = user;
     }
 
-    public List<Category> getCategories() {
+    public Set<Category> getCategories() {
         return categories;
     }
 
-    public void setCategories(List<Category> categories) {
+    public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
 
