609855c3a8d1cc0d1f1c68e246d6dc78def67566
alexvolchkov <volkaa@mail.ru>
2022-06-26 16:56:22 +0300

    4. Категории в TODO List [#331991 #315483]

--- src/main/java/ru/job4j/todo/filter/AuthFilter.java
+++ src/main/java/ru/job4j/todo/filter/AuthFilter.java
@@ -1,10 +1,13 @@
 package ru.job4j.todo.filter;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class AuthFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -18,7 +21,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse res = (HttpServletResponse) servletResponse;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+        if (uri.endsWith("loginPage") || uri.endsWith("login") || uri.endsWith("index")) {
             filterChain.doFilter(req, res);
             return;
         }
